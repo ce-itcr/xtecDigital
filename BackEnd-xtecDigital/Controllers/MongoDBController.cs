20342dcf2d65da1a25f238971041abfc4642497b
Iván <isolis@solavcr.com>
2020-12-05 00:56:00 -0600

    [ADD] MongoDB add and find methods

---
+++ BackEnd-xtecDigital/Controllers/MongoDBController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace BackEnd_xtecDigital.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200/", headers: "*", methods: "*")]
+    public class MongoDBController : ApiController
+    {
+
+        [HttpPost]
+        [Route("api/MongoTest")]
+        public IHttpActionResult mongoTest([FromBody] JObject users)
+        {
+            Console.WriteLine(7);
+            Models.MongoConnection mongo = new Models.MongoConnection();
+            Console.WriteLine(6);
+            Console.WriteLine(8);
+            //return Ok(testMongo.findWithFilter("Nombre", "agustin"));
+            //return Ok(testMongo.findAll());
+            //testMongo.insertStudent(2018209698, "Ivan", "ivan@gmail.com", 88888888, "pass");
+            return Ok();
+        }
+    }
+}
---
+++ BackEnd-xtecDigital/Models/MongoConnection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BackEnd_xtecDigital.Models
+{
+    public class MongoConnection
+    {
+        public JObject findAll()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:<SPyNGA7F>@cluster0.8aanu.mongodb.net/<xtecDigital>?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+            var db = dbClient.GetDatabase("DBaaS_Taller");
+            var col = db.GetCollection<BsonDocument>("Notas").Find(new BsonDocument()).ToList();
+            JObject obj = new JObject();
+            int x = 1;
+            foreach (BsonDocument doc in col)
+            {
+                JProperty property = new JProperty("user" + x.ToString(), JObject.Parse(doc.ToJson()));
+                obj.Add(property);
+                x++;
+            }
+            return obj;
+        }
+
+        public JObject findWithFilter(string attribute, string value)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:<SPyNGA7F>@cluster0.8aanu.mongodb.net/<xtecDigital>?retryWrites=true&w=majority");
+            var db = dbClient.GetDatabase("DBaaS_Taller");
+            var filter = Builders<BsonDocument>.Filter.Eq(attribute, value);
+            var col = db.GetCollection<BsonDocument>("Notas");
+            var res = col.Find(filter).FirstOrDefault().ToJson();
+            Debug.Print(res);
+            return JObject.Parse(res);
+        }
+
+        public void insertStudent(int id, string name, string email, int tel, string password)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<bname>?retryWrites=true&w=majority");
+            var db = dbClient.GetDatabase("xtecDigital");
+            var col = db.GetCollection<BsonDocument>("Students");
+            string encryptedPass = MD5Encoding.MD5Encryption(password);
+            var doc = new BsonDocument
+            {
+                {"Id", id},
+                {"Name", name},
+                {"Email", email},
+                {"PhoneNumber", tel},
+                {"Password", encryptedPass}
+            };
+            col.InsertOne(doc);
+        }
+    }
+}
