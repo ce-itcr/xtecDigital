f93132eb63bdb04d7b302124771f8d21dde65447
Iván <isolis@solavcr.com>
2020-12-05 18:23:14 -0600

    [UPDATE] MongoDB finished

--- BackEnd-xtecDigital/Models/MongoConnection.cs
+++ BackEnd-xtecDigital/Models/MongoConnection.cs
@@ -12,47 +12,101 @@ namespace BackEnd_xtecDigital.Models
 {
     public class MongoConnection
     {
-        public JObject findAll()
+        public JObject findAllStudents()
         {
-            MongoClient dbClient = new MongoClient("mongodb+srv://admin:<SPyNGA7F>@cluster0.8aanu.mongodb.net/<xtecDigital>?retryWrites=true&w=majority");
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
 
             var dbList = dbClient.ListDatabases().ToList();
-            var db = dbClient.GetDatabase("DBaaS_Taller");
-            var col = db.GetCollection<BsonDocument>("Notas").Find(new BsonDocument()).ToList();
+            var db = dbClient.GetDatabase("xtecDigital");
+            var col = db.GetCollection<BsonDocument>("Students").Find(new BsonDocument()).ToList();
             JObject obj = new JObject();
+            Debug.Print(db.GetCollection<BsonDocument>("Students").Find(new BsonDocument()).ToList()[0].ToJson());
             int x = 1;
             foreach (BsonDocument doc in col)
             {
-                JProperty property = new JProperty("user" + x.ToString(), JObject.Parse(doc.ToJson()));
+                JProperty property = new JProperty("Student" + x.ToString(), JObject.Parse(doc.ToJson()));
                 obj.Add(property);
                 x++;
             }
             return obj;
         }
 
-        public JObject findWithFilter(string attribute, string value)
+        public JObject findAllTeachers()
         {
-            MongoClient dbClient = new MongoClient("mongodb+srv://admin:<SPyNGA7F>@cluster0.8aanu.mongodb.net/<xtecDigital>?retryWrites=true&w=majority");
-            var db = dbClient.GetDatabase("DBaaS_Taller");
-            var filter = Builders<BsonDocument>.Filter.Eq(attribute, value);
-            var col = db.GetCollection<BsonDocument>("Notas");
-            var res = col.Find(filter).FirstOrDefault().ToJson();
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+            var db = dbClient.GetDatabase("xtecDigital");
+            var col = db.GetCollection<BsonDocument>("Teachers").Find(new BsonDocument()).ToList();
+            JObject obj = new JObject();
+            int x = 1;
+            foreach (BsonDocument doc in col)
+            {
+                JProperty property = new JProperty("Teacher" + x.ToString(), JObject.Parse(doc.ToJson()));
+                obj.Add(property);
+                x++;
+            }
+            return obj;
+        }
+
+        public JObject getStudentData(JObject json)
+        {
+            Debug.Print(json.ToString());
+            Debug.Print(json["id"].ToString());
+            Debug.Print(json["pass"].ToString());
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
+            var db = dbClient.GetDatabase("xtecDigital");
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("_id", json["id"]) & builder.Eq("Password", json["pass"]);
+            var col = db.GetCollection<BsonDocument>("Students");
+            var res = col.Find(new BsonDocument()).FirstOrDefault().ToJson();
             Debug.Print(res);
             return JObject.Parse(res);
         }
 
-        public void insertStudent(int id, string name, string email, int tel, string password)
+        public JObject getTeacherData(JObject json)
         {
-            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<bname>?retryWrites=true&w=majority");
+            Debug.Print(json.ToString());
+            Debug.Print(json["id"].ToString());
+            Debug.Print(json["pass"].ToString());
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
+            var db = dbClient.GetDatabase("xtecDigital");
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("_id", json["id"]) & builder.Eq("Password", json["pass"]);
+            var col = db.GetCollection<BsonDocument>("Teachers");
+            var res = col.Find(new BsonDocument()).FirstOrDefault().ToJson();
+            Debug.Print(res);
+            return JObject.Parse(res);
+        }
+
+        public void insertStudent(JObject json)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
             var db = dbClient.GetDatabase("xtecDigital");
             var col = db.GetCollection<BsonDocument>("Students");
-            string encryptedPass = MD5Encoding.MD5Encryption(password);
+            string encryptedPass = MD5Encoding.MD5Encryption(json["pass"].ToString());
             var doc = new BsonDocument
             {
-                {"Id", id},
-                {"Name", name},
-                {"Email", email},
-                {"PhoneNumber", tel},
+                {"_id", (int)json["id"]},
+                {"Name", json["name"].ToString()},
+                {"Email", json["email"].ToString()},
+                {"PhoneNumber", (int)json["phone"]},
+                {"Password", encryptedPass}
+            };
+            col.InsertOne(doc);
+        }
+
+        public void insertTeacher(JObject json)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:admin@cluster0.8aanu.mongodb.net/<dbname>?retryWrites=true&w=majority");
+            var db = dbClient.GetDatabase("xtecDigital");
+            var col = db.GetCollection<BsonDocument>("Teachers");
+            string encryptedPass = MD5Encoding.MD5Encryption(json["pass"].ToString());
+            var doc = new BsonDocument
+            {
+                {"_id", (int)json["id"]},
+                {"Name", json["name"].ToString()},
+                {"Email", json["email"].ToString()},
                 {"Password", encryptedPass}
             };
             col.InsertOne(doc);
