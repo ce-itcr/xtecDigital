76a5c73f46c72082523f1b02ec23b87fa46ece9f
Iván <isolis@solavcr.com>
2021-01-05 23:16:39 -0600

    [UPDATE] Mongo

---
+++ BackEnd-xtecDigital/Models/MongoStudent.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_xtecDigital.Models
+{
+    [BsonIgnoreExtraElements]
+    public class MongoStudent
+    {
+        [BsonId]
+        public int _id { get; set; }
+        public string Name { get; set; } 
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
